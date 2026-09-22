e4078cb8304d3983fd581f8b5b14355cdc4166df
Vishnu <vishnuyadhav519@gmail.com>
2026-09-23 00:10:21 +0530

    Connect frontend to deployed backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API_URL = (process.env.REACT_APP_API_URL || "http://localhost:3001").replace(/\/$/, "");
+const API_URL = (
+  process.env.REACT_APP_API_URL ||
+  "https://vishnu18-event-booking.onrender.com"
+).replace(/\/$/, "");
 
 const api = axios.create({
   baseURL: `${API_URL}/V1/api`,
@@ -8,11 +11,14 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
 export { API_URL };
+
 export default api;
